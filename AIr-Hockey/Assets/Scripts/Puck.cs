189397e557f3dd192a655112622f8e7d0a03d24d
luka hendrickx <luka.hendrickx@gmail.com>
2020-12-14 08:57:53 +0100

    Pushes

--- AIr-Hockey/Assets/Scripts/Puck.cs
+++ AIr-Hockey/Assets/Scripts/Puck.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Puck : MonoBehaviour
 {
-    public Rigidbody rb;
-
+    private Environment environment;
+    private Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        environment = GetComponentInParent<Environment>();
     }
 
     void Update()
     {
         
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Field"))
@@ -29,8 +31,26 @@ public class Puck : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Border"))
         {
-            rb.velocity = Vector3.Reflect(collision.relativeVelocity * 2, collision.contacts[0].normal);
+            var orthogonalVector = collision.contacts[0].point - transform.position;
+            var collisionAngle = Vector3.Angle(orthogonalVector, rb.velocity);
+            transform.rotation *= Quaternion.Euler(0, collisionAngle, 0);
+            rb.velocity = new Vector3(rb.velocity.x * 2, 0, rb.velocity.z * 2);
+            //rb.velocity = Vector3.Reflect(collision.relativeVelocity * 2, collision.contacts[0].normal);
             //rb.velocity = new Vector3(25, 0, 25);
+        }
+        if (collision.gameObject.CompareTag("GoalPlayer"))
+        {
+            environment.AddPointsOpponent();
+            rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            environment.ClearEnvironment(false);
+        }
+        if (collision.gameObject.CompareTag("GoalOpponent"))
+        {
+            environment.AddPointsPlayer();
+            rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            environment.ClearEnvironment(true);
         }
     }
 }
